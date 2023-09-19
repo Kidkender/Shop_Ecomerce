e5b056981dcff40424fc86fe940965cf3c58c46f
kidkender <kidkender@gmail.com>
2023-09-19 23:51:31 +0700

    modify admin page

---
+++ src/components/admin/orders/Order.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./AddProduct.module.scss";
+
+const cx = classNames.bind(styles);
+
+const AddProduct = () => {
+  return <div></div>;
+};
